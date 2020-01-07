067acf077d44eb47e9ffd28007636f54d4066bd0
”名前” <>
2020-01-07 08:26:39 +0000

    課題提出

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('profile/create', 'Admin\ProfileController@add');
-
     Route::get('profile/edit', 'Admin\ProfileController@edit');
-
+    Route::post('news/create', 'Admin\NewsController@create');
 });
 
-
-
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
 

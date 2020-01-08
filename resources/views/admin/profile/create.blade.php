9fa1299fb18c76c954292886d4874478ac732b17
”名前” <>
2020-01-08 13:57:27 +0000

    課題提出１３

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -16,5 +16,56 @@
         </div>
     </div>
 @endsection
-   
+   <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">タイトル</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">本文</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">画像</label>
+                        <div class="col-md-10">
+                            <input type="file" class="form-control-file" name="image">
+                        </div>
+                    <div class="form group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                        <input type="text" id="name" name="name">
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                    <div class="col-md-10">
+                        <input type="radio" name="性別" value="男" /> 男　
+　　　　　　　　　　　　<input type="radio" name="性別" value="女" /> 女
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                    　　<input type="text" id="hobby" name="hobby">
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <input type="text" id="introduction" name="introduction">
+                    </div>
+                    </div>
+                    
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
+                  </div>
+            
 
